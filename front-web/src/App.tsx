2102a26a65f79d505d5bca8fe160634442b1ed5c
Fellipe Martins de Sousa <fellipems14@gmail.com>
2021-01-08 20:58:57 -0300

    Creat front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Hello message="Fellipe" />
+      <Hello message="SDS" />
+      <Hello message="DS DELIVER" />
+      <Hello message="Props" />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-web/src/Hello.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    message: string;
+}
+
+function Hello({message}: Props) {  // um componente função, o nome desse componente, sempre retornará elementos HTML ou outros componentes
+    return (
+        //<h1>Component Hello</h1>
+        <h1>Hello {message} !</h1>
+    )
+}
+
+export default Hello;   // exportando por default todo componente Hello
+
+// Prop no react são os parâmetros que passamos em uma função, para uma função que é component
